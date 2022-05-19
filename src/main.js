7829a686916a076ece49d4b56d116f588c69fd0d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-18 23:03:02 -0700

    add business and UI logic

---
+++ src/main.js
@@ -0,0 +1,49 @@
+// This function stores our state.
+
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+const stateControl = storeState();
+
+// This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
+
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  }
+}
+
+// We create four functions using our function factory. We could easily create many more.
+
+const feed = changeState("soil")(1);
+const blueFood = changeState("soil")(5);
+
+const hydrate = changeState("water")(1);
+const superWater = changeState("water")(5);
+
+$(document).ready(function() {
+
+// This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
+
+  $('#feed').click(function() {
+    const newState = stateControl(blueFood);
+    $('#soil-value').text(`Soil: ${newState.soil}`);
+  });
+
+// This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
+
+  $('#show-state').click(function() {
+    // We just need to call stateControl() without arguments to see our current state.
+    const currentState = stateControl();
+    $('#soil-value').text(`Soil: ${currentState.soil}`);
+  });
+});
